d485dffba5e136c4cb45007d46172857fdcf737a
George <bumgardnera07@gmail.com>
2017-05-30 10:16:07 -0400

    checkpoint 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function(){
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points){
 
   var revealPoint = function(j) {
      points[j].style.opacity = 1;
@@ -12,3 +12,14 @@ var animatePoints = function(){
       revealPoint(i);
     }
 };
+
+window.onload = function() {
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+      }
+    });
+}
