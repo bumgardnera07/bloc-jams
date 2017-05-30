2db6d2eb6f599a014fcd2f8e4c1b37dbf2428f60
George <bumgardnera07@gmail.com>
2017-05-30 10:43:01 -0400

    assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,12 +8,14 @@ var animatePoints = function(points){
      points[j].style.msTransform = "scaleX(1) translateY(0)";
      points[j].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-    for (var i= 0; i< points.length; i++) {
-      revealPoint(i);
-    }
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
+
+  if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
    var sellingPoints = document.getElementsByClassName('selling-points')[0];
    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
