e1f4a7558e9917a2816840345d368c2c4110ada2
George <bumgardnera07@gmail.com>
2017-06-13 23:10:28 -0400

    assignment-20-donezo

--- scripts/album.js
+++ scripts/album.js
@@ -151,6 +151,23 @@ var setSong = function(songNumber) {
 var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
 }
+
+var togglePlayFromPlayerBar= function() {
+  var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+  if (currentSoundFile.isPaused()) {
+    $currentlyPlayingCell.html(pauseButtonTemplate);
+    $playBarButton.html(playerBarPauseButton);
+    currentSoundFile.play();
+  }
+  else if (currentSoundFile != null) {
+    $currentlyPlayingCell.html(playButtonTemplate);
+    $playBarButton.html(playerBarPlayButton);
+    currentSoundFile.pause();
+  }
+}
+
+
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -162,6 +179,8 @@ var currentSongFromAlbum = null;
 var currentSoundFile = null;
 var currentVolume = 80;
 
+var $playBarButton = $('.main-controls .play-pause')
+
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 
@@ -169,4 +188,5 @@ $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playBarButton.click(togglePlayFromPlayerBar)
 });
